9f4f2ae61e92e9309d02eb833183c744c0b92d23
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-03-25 21:40:00 +0000

    delete other code

--- splut/delay.py
+++ splut/delay.py
@@ -1,19 +1,19 @@
 # Copyright 2014, 2018, 2019 Andrzej Cichocki
 
-# This file is part of pym2149.
+# This file is part of splut.
 #
-# pym2149 is free software: you can redistribute it and/or modify
+# splut is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pym2149 is distributed in the hope that it will be useful,
+# splut is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
+# along with splut.  If not, see <http://www.gnu.org/licenses/>.
 
 from splut.bg import SimpleBackground
 from diapyr import types
