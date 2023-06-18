ca7482d51d21424851274347d770db39e687906a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2023-06-18 09:32:42 +0100

    refactoring

--- splut/actor/__init__.py
+++ splut/actor/__init__.py
@@ -18,6 +18,7 @@
 from .future import Future
 from .mailbox import Mailbox
 from .message import Message
+from functools import partial
 
 class Spawn:
 
@@ -25,11 +26,11 @@ class Spawn:
         self.executor = executor
 
     def __call__(self, *objs):
+        def post(name, *args, **kwargs):
+            future = Future()
+            mailbox.add(Message(name, args, kwargs, future))
+            return future
         def __getattr__(self, name):
-            def post(*args, **kwargs):
-                future = Future()
-                mailbox.add(Message(name, args, kwargs, future))
-                return future
-            return post
+            return partial(post, name)
         mailbox = Mailbox(self.executor, objs)
         return type(f"{''.join({type(obj).__name__: None for obj in objs})}Actor", (), {f.__name__: f for f in [__getattr__]})()
